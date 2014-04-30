49ec1aa407690a3b6a728c10ce09bd2e416de2e1
spaundt <spaun1991t@gmail.com>
2014-04-30 03:30:26 +0300

    added server class

---
+++ Server/Server/LdpServer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class LdpServer : ILdpServer
+    {
+        public LdpServer() { }
+
+        public void StartServer(int port)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopServer()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
