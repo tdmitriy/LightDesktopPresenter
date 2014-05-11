c190dd813cedf12765338330c8462a7747a77abf
m1tr14 <spaun1991t@gmail.com>
2014-05-11 02:19:32 +0200

    d

--- Server/Server/Network/PacketTypes/LdpAuthRequest.cs
+++ Server/Server/Network/PacketTypes/LdpAuthRequest.cs
@@ -5,12 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using ProtoBuf;
 
-namespace Server.Network
+namespace Server.Network.PacketTypes
 {
     [ProtoContract]
     class LdpAuthRequest
     {
         [ProtoMember(1)]
         public string Password { get; set; }
+       
     }
+
+   
 }
