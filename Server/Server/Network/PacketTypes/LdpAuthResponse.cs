ca42bb5d61f746bcb1a9603f3957a5eb2754eb9b
m1tr14 <spaun1991t@gmail.com>
2014-05-15 00:29:54 +0200

    deleted some fileds

--- Server/Server/Network/PacketTypes/LdpAuthResponse.cs
+++ Server/Server/Network/PacketTypes/LdpAuthResponse.cs
@@ -12,14 +12,5 @@ namespace Server.Network.PacketTypes
     {
         [ProtoMember(1, IsRequired = true)]
         public bool isSuccess { get; set; }
-        [ProtoMember(2, IsRequired = true)]
-        public ResponseMessageType Type { get; set; }
-
-        
-    }
-    enum ResponseMessageType
-    {
-        WRONG_PASSWORD = 1,
-        EMPTY_PASSWORD = 2,
     }
 }
