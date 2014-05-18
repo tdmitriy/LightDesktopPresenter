7ab6c8a4e78a58e4d66eec2488b8f5ae9aaeace5
m1tr14 <spaun1991t@gmail.com>
2014-05-18 16:59:39 +0300

    project structure refactoring

--- AndroidClient/LdpAndroidClient/app/src/main/java/com/ldp/androidclient/activities/ActivityMain.java
+++ AndroidClient/LdpAndroidClient/app/src/main/java/com/ldp/androidclient/activities/ActivityMain.java
@@ -20,18 +20,13 @@ public class ActivityMain extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main_menu_action, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        switch(id) {
+        switch(item.getItemId()) {
             case R.id.settings_add_new_connection:
                 //Toast.makeText(this, "Add new coonection clicked", Toast.LENGTH_SHORT).show();
                 Intent newConnectionActivity = new Intent(this, ActivityNewConnection.class);
