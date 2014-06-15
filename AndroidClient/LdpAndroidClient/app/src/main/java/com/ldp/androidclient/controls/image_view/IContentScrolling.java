6018a157d909152036aacc1bffa0c6545bd0bb70
m1tr14 <spaun1991t@gmail.com>
2014-06-15 19:18:07 +0300

    some refactoring

--- AndroidClient/LdpAndroidClient/app/src/main/java/com/ldp/androidclient/controls/image_view/IContentScrolling.java
+++
@@ -1,5 +0,0 @@
-package com.ldp.androidclient.controls.image_view;
-
-public interface IContentScrolling {
-    void scrollContentTo(int x, int y);
-}
--- AndroidClient/LdpAndroidClient/app/src/main/java/com/ldp/androidclient/controls/image_view/ICursorView.java
+++
@@ -1,6 +0,0 @@
-package com.ldp.androidclient.controls.image_view;
-
-public interface ICursorView {
-    float getCursroX();
-    float getCursorY();
-}
